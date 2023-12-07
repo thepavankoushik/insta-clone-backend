59c0c244a41929b3f294f50d4564c3c49bf8b3b5
thepavankoushik <pavankoushik22@gmail.com>
2023-12-06 17:40:28 -0800

    add cors

--- main.py
+++ main.py
@@ -6,15 +6,31 @@ from routers.post import router as PostRouter
 from fastapi.staticfiles import StaticFiles
 from auth.authentication import router as AuthRouter
 from routers.comment import router as CommentRouter
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
 app.include_router(UserRouter)
 app.include_router(PostRouter)
 app.include_router(AuthRouter)
 app.include_router(CommentRouter)
 
+
+
 @app.get("/")
 def root():
     return {"message": "Hello World"}
+
+origins = [
+    'http://localhost:3000'
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
 
 models.Base.metadata.create_all(bind=engine)
 
